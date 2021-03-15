e66fbc350e4a65c88d9be354e6f9c55ada0e9144
ikaraoz <ismail.karaoz@ppmap.net>
2021-03-15 21:26:21 +0000

    Added methods of extracting data from response body and uploading files as multipart/form-data

--- src/test/java/restAssured/TechBeaconSampleProject.java
+++ src/test/java/restAssured/TechBeaconSampleProject.java
@@ -4,6 +4,7 @@ import io.restassured.builder.ResponseSpecBuilder;
 import io.restassured.http.ContentType;
 import io.restassured.response.Response;
 import io.restassured.specification.ResponseSpecification;
+import org.junit.jupiter.api.Assertions;
 import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 
@@ -18,10 +19,12 @@ import org.testng.annotations.Test;
  */
 
 
+import java.io.File;
+
 import static io.restassured.RestAssured.*;
 import static org.hamcrest.Matchers.*;
 
-public class ExerciseRestAssured {
+public class TechBeaconSampleProject {
 
     ResponseSpecification checkStatusCodeAndContentType =
             new ResponseSpecBuilder().
@@ -30,16 +33,89 @@ public class ExerciseRestAssured {
                     build();
 
 
+    // Check whether response size equals expected
     @Test
     public void test_NumberOfCircuitsFor2017Season_ShouldBe20() {
-        given().
-                when().
-                get("http://ergast.com/api/f1/2017/circuits.json").
-                then().
-                assertThat().
-                body("MRData.CircuitTable.Circuits.circuitId", hasSize(20)).log().all();
+//        given().
+//                when().
+//                get("http://ergast.com/api/f1/2017/circuits.json").
+//                then().
+//                assertThat().
+//                body("MRData.CircuitTable.Circuits.circuitId", hasSize(20));
+
+        //Bu methodun yukaridakinden farki, url'inin baseUri ve basePath olarak ayri yazilmis olmasi
+        //Bir alternatif gostermesi icin yazdim.
+        given()
+                .baseUri("http://ergast.com/api")
+                .basePath("/f1/2017/circuits.json")
+                .get()
+                .then()
+                .assertThat()
+                .body("MRData.CircuitTable.Circuits.circuitId", hasSize(20)).log().all();
     }
 
+    // Extract a field value from the response E.g. 1
+    @Test
+    public void test_NumberOfCircuitsFor2017Season_ShouldBe20_alternative() {
+        Response response = given().
+                when().
+                get("http://ergast.com/api/f1/2017/circuits.json");
+
+        response.then().
+                assertThat().
+                body("MRData.CircuitTable.Circuits.circuitId", hasSize(20)).log().all();
+
+        //Get a specific circuit ID
+        String circuitId = response
+                .then()
+                .extract()
+                .path("MRData.CircuitTable.Circuits[0].circuitId");
+
+        System.out.println("circuitId = " + circuitId);
+    }
+
+    //bu method ile, yaptigimiz Get request sonrasi donen response body icerisinden
+    // firstname field'indaki value'yu almis(extract) etmis olduk.
+    @Test
+    public void getCheckInDate() {
+        String firstname = given()
+                .baseUri("https://restful-booker.herokuapp.com")
+                .basePath("/booking/1")
+                .get()
+                .then()
+                .statusCode(200)
+                .extract()
+                .path("firstname");
+
+        System.out.println("firstname = " + firstname);
+    }
+
+
+    // Extract a field value from the response E.g. 2
+    //Bu metod icerisinde donen response body'den token'i aliyoruz.
+    @Test
+    public void getNewToken() {
+        Response response = given().
+                baseUri("https://restful-booker.herokuapp.com").
+                basePath("/auth").
+                contentType("application/json").
+                body("{\n" +
+                        "    \"username\": \"admin\",\n" +
+                        "    \"password\": \"password123\"\n" +
+                        "}").
+                post();
+// body() is used with POST, PUT, DELETE requests
+        String token = response
+                .then()
+                .extract()
+                .path("token");
+        System.out.println("token = " + token);
+
+        //verifying response token
+        Assertions.assertTrue(token=="123");
+    }
+
+    // Verifying response data
     @Test
     public void test_ResponseHeaderData_ShouldBeCorrect() {
         given().
@@ -52,6 +128,42 @@ public class ExerciseRestAssured {
                 contentType(ContentType.JSON).
                 and().
                 header("Content-Length", equalTo("4551"));
+    }
+
+    //Bu method, response time i, expected response time ile karsilastirabilmek icin;
+    @Test
+    public void measureResponseTime() {
+
+        //URL'e get request'i gonderip donen response'u response variable'i icine tanimladik
+        Response response = given().
+                when().
+                get("http://ergast.com/api/f1/2017/circuits.json");
+
+        //Yukarida tanimlanan response'dan, bu response'un donmesinin ne kadar surdugunu aldik ve yazdirdik gormek icin.
+        long timeInMs = response.time();
+        System.out.println("timeInMs = " + timeInMs);
+
+        //Burada ise donen response 2000L milisaniye (2 saniye)'ye esit ya da daha az mi diye karsilastiriyoruz.
+        response.
+                then().
+                time(lessThanOrEqualTo(2000L));
+
+    }
+
+    //upload file
+    @Test
+    public void uploadFile() {
+        given()
+                .baseUri("https://petstore.swagger.io/v2/pet/{petId}/uploadImage")
+                .basePath("/pet/{petId}/uploadImage")
+                .pathParam("petId", 12121212)
+                .multiPart(new File("src/test/resources/mavi.jpeg"))
+                .post()
+                //.post("https://petstore.swagger.io/v2/pet/12121212/uploadImage")
+                .then()
+                .statusCode(200)
+                .log()
+                .all();
     }
 
     @Test
