4f332a8808c08d49d404aae7b7ae891f83c08426
ikaraoz <ismail.karaoz@ppmap.net>
2021-03-19 10:47:11 +0000

    Added petStore feature file and step definitions

--- src/test/java/restAssured/RunCucumberTest.java
+++ src/test/java/restAssured/RunCucumberTest.java
@@ -6,16 +6,16 @@ import org.junit.runner.RunWith;
 
 @RunWith(Cucumber.class)
 @CucumberOptions(
-        features = {"src/test/resources/end2end.feature"}, // this finds the feature file that we want to run
+        features = {"src/test/resources/testBookingApp.feature"}, // this finds the feature file that we want to run
         plugin = {"pretty", //to pretty print in the console
                 "summary", // for a summary of the test run
-                "html:target/cucumber.html", // one option for reporting the test results
+                //"html:target/cucumber.html", // one option for reporting the test results
                 // This creates a target folder where you can find cucumber.html to view the test results
                 "de.monochromata.cucumber.report.PrettyReports:result/cucumber" // another option to report test results
                 // This creates a result folder where you can find cucumber report to view the test results
 
         },
-        tags = "@Unhappy" // this is to run only the tags given here.
+        tags = "@Filter" // this is to run only the tags given here.
 )
 public class RunCucumberTest {
 }
